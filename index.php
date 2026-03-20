7b551490b044c46ef6439ad74692a0e39eba562f
schoolGit03 <schoolassignment03@gmail.com>
2026-03-20 16:11:12 +0530

    Test vulnerability CORRECT

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $res = $db->query("SELECT jokes.*, users.username
 FROM jokes JOIN users ON users.id=jokes.user_id 
 ORDER BY $sort");
 
-$querySecurityTest = "SELECT * FROM users WHERE username='$_POST[user]'";
+// $querySecurityTest = "SELECT * FROM users WHERE username='$_POST[user]'";
 
 ?>
 
